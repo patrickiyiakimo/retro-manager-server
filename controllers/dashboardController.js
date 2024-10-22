95b48031ab3148371b0d08bb033bd183d08aca82
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-22 16:26:18 +0100

    created an API end point for the dashboard

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,14 +1,21 @@
 const pool = require("../config/db");
 require("dotenv").config();
 
-
 const dashboard = async (req, res) => {
-    try {
-        const { dashboard_id } = req.params
-        
-        await pool.query ()
-    } catch (error) {
-        console.error(error.message)
-        res.sendStatus(400) //Bad request
-    }
-}
+  try {
+    const { team_name, invited_at, participants_count } = req.body;
+
+    const result = await pool.query(
+      "INSERT INTO dashboard (team_name, invited_at, participants_count) VALUES ($1, $2, $3) RETURNING *",
+      [team_name, invited_at, participants_count]
+    );
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+    res.sendStatus(400); // Bad request
+  }
+};
+
+module.exports = {
+  dashboard,
+};

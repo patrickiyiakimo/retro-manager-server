09e6287db9ba317999b50820041cc3aecc26f071
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-28 10:47:21 +0100

    cleared some unnecessary comments in the invites.js file

--- routes/invites.js
+++ routes/invites.js
@@ -4,8 +4,8 @@ const inviteTeamsControllers = require("../controllers/inviteTeamsControllers");
 
 router.post("/", inviteTeamsControllers.invite_team);
 router.get("/", inviteTeamsControllers.get_invitations);
-router.put("/accept", inviteTeamsControllers.accept_invitation); // Changed to accept
-router.put("/decline", inviteTeamsControllers.decline_invitation); // Changed to decline
+router.put("/accept", inviteTeamsControllers.accept_invitation);
+router.put("/decline", inviteTeamsControllers.decline_invitation); 
 router.delete("/", inviteTeamsControllers.delete_invitation);
 
-module.exports = router; // Corrected the export
+module.exports = router; 
